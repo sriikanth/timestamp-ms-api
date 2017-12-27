f202e153c38f991fcb8ce8b67004313d79a75921
Srikanth <srikanth.sekar@yahoo.com>
2017-12-27 20:15:21 +0530

    Files modifies (#2)

--- app/api/timestamp.js
+++ app/api/timestamp.js
@@ -1,5 +1,5 @@
 'use strict';
-var moment = require('moment');
+//var moment = require('moment');
 
 module.exports = function(app) {
 
@@ -7,34 +7,35 @@ module.exports = function(app) {
         var date = req.params.query;
         var unix = null;
         var natural = null;
-        
+
         // Check for initial unix time
         if (+date >= 0) {
             unix = +date;
             natural = unixToNat(unix);
-        } 
-        
+        }
+
         // Check for initial natural time
         if (isNaN(+date) && moment(date, "MMMM D, YYYY").isValid()) {
             unix = +natToUnix(date);
-            natural = unixToNat(unix);
+            //natural = unixToNat(unix);
         }
-        
+
         var dateObj = { "unix": unix, "natural": natural };
         res.send(dateObj);
-        
+
     });
-    
+
     function natToUnix(date) {
         // Conver from natural date to unix timestamp
-        return moment(date, "MMMM D, YYYY").format("X");
+        //return moment(date, "MMMM D, YYYY").format("X");
+        return Date.now();
     }
-    
-    function unixToNat(unix) {
+
+    /*function unixToNat(unix) {
         // Convert unix timestamp to natural date
         return moment.unix(unix).format("MMMM D, YYYY");
-    }
-    
-    
-    
+    }*/
+
+
+
 };
